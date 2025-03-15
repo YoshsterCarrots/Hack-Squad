299c3f5ad324317d28179c94b16d9290edc11aca
wasserman_1005519 <1005519@my.scccd.edu>
2025-03-15 08:50:53 -0700

    Wasserman commit

--- particle_graphics.h
+++ particle_graphics.h
@@ -13,12 +13,11 @@ class ParticleGraphics {
 
 	public:
 	
-	ParticleGraphics() : r() g() b() {}
+	ParticleGraphics(int red = 0, int green = 0, int blue = 0) : r(red) g(green) b(blue) {}
 
 	void getColor() const {
 
 	}
-
 	void setColor(int red, int green, int blue) {
 
 		r = red;
@@ -26,5 +25,22 @@ class ParticleGraphics {
 		b = blue;
 
 	}
+	void drawOval(int x, int y,int width,int height) {
+	
+	}
+	void drawPoint(int x, int y) {
+
+	}
+	void drawRectangle(int x, int y,int width,int height) {
+
+	}
+	void drawPolygon(int numPoints) {
+
+	}
+	void drawLine(int x1, int y1, int x2, int y2) {
+
+	} 
 
 
+
+}; 
