f983102032d639f921a26be750b6e03020eccb26
wasserman_1005519 <1005519@my.scccd.edu>
2025-03-19 18:57:49 -0700

    Last milestone

--- particle_graphics.h
+++ particle_graphics.h
@@ -27,8 +27,22 @@ class ParticleGraphics {
 		b = blue;
 
 	}
-	void drawOval(int x, int y,int width,int height) {
-	
+	void drawCircle(int x, int y, int radius, int red, int green, int blue) {
+		assert(radius > 0);
+		resetcolor();
+		setbgcolor(red, green, blue);
+		for (int row = x - radius; row <= x + radius; row++) {
+			for (int col = y - radius; col <= y + radus; col++) {
+				int dx = row - x;
+				int dy = col - y;
+				if (dx * dx + dy * dy <= radius * radius) {
+					movecursor(row,col);
+					cout << " ";
+				}
+			}
+		}
+		cout.flush();
+		resetcolor();
 	}
 	void drawPoint(int row, int col, red, green, blue) {
 		assert( row >= 0 and col >= 0);
@@ -39,14 +53,48 @@ class ParticleGraphics {
 		cout.flush();
 		resetcolor();
 	}
-	void drawRectangle(int x, int y,int width,int height) {
-
+	void drawRectangle(int min_x, int min_y, int max_x, int max_y, int red, int green, int blue) {
+		assert(min_x < max_x);
+		assert(min_y < max_y);
+		resetcolor();
+		setbgcolor(red, green, blue);
+		for (int row = min_x; row <= max_x; row++) {
+			for (int col = min_y; row <= max_y; col++) {
+				movecursor(row,col);
+				cout << " ";
+			}
+		{
+		cout.flush();
+		resetcolor();
 	}
-	void drawPolygon(int numPoints) {
 
-	}
-	void drawLine(int x1, int y1, int x2, int y2) {
+	void drawLine(int x1, int y1, int x2, int y2, int red, int green, int blue) {
+		assert(x1 >= 0 and x2 >= 0 and y1 >= 0 and y2 >= 0);
+		assert((x1 != x2) and (y1 != y2));
+		resetcolor();
+		setbgcolor(red, green, blue);
+		if (x1 == x2) { //Verticle Line
+			int y_start = (y1 < y2) ? y1 : y2;
+			int y_end = (y1 > y2) ? y1 : y2;
+			for (int y = y_start; y <= y_end; y++) {
+				movecursor(x1, y);
+				cout << " ";
+			}
 
+		}
+		else if (y1 == y2) {
+			int x_start = (x1 < x2) ? x1 : x2;
+			int x_end = (x1 > x2) ? x1 : x2;
+			resetcolor();
+			setbgcolor(red, green, blue);
+			for (int x = x_start; x <= x_end; x++) {
+				movecursor(x, y1);
+				cout << " ";
+			}
+		}
+
+		cout.flush();
+		resetcolor();
 	} 
 
 
