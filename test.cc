ff9b30fe6667786d2066118ada3e75400ff427c3
0992363 <0992363@my.scccd.edu>
2025-03-21 14:17:19 -0700

    more tests made

--- test.cc
+++ test.cc
@@ -20,6 +20,7 @@ void die(string s) {
 void particle_test() {
 	Particle p;
 	ParticleGraphics pg;
+
 	if (p.getX() || p.getY() || p.getXVel() != 1 || p.getYVel() != 1 || p.getLifetime() != 100 || p.getType() != Particle::STREAMER) die("Particle default constructor ain't working");
 	if (pg.getColor('r') || pg.getColor('g') || pg.getColor('b')) die("Particle Graphics default constructor ain't working");
 	pg.setColor(25, 15, 245);
@@ -39,10 +40,19 @@ void particle_test() {
 	p.doPhysics(1, 1);
 	if (p.getX() != 7 || p.getY() != 16 || p.getXVel() != 3 || p.getYVel() != 6 || p.getLifetime() != 399) die("Physics ain't working");
 	p.setType(Particle::BALLISTIC);
+	if (p.getType() != Particle::BALLISTIC) die("Setting Type ain't working");
 	p.doPhysics(2, 7);
 	if (p.getX() != 12 || p.getY() != 30 || p.getXVel() != 5 || p.getYVel() != 14 || p.getLifetime() != 398) die("Ballistic Physics ain't working");
-}
+	
+//	pg.drawPoint(p.getX(), p.getY(), pg.getColor('r'), pg.getColor('g'), pg.getColor('b'));
 
+	Particle l(2, 4, 1, 5, 350, Particle::FIREWORK);
+	ParticleGraphics cool(42, 69, 255);
+	if (l.getX() != 2 || l.getY() != 4 || l.getXVel() != 1 || l.getYVel() != 5 || l.getLifetime() != 350 || l.getType() != Particle::FIREWORK) die("Particle constructor ain't working");
+	if (cool.getColor('r') != 42 || cool.getColor('g') != 69 || cool.getColor('b') != 255) die ("Particle Graphic constructor ain't working");
+
+	cool.drawPoint(l.getX(), l.getY(), cool.getColor('r'), cool.getColor('g'), cool.getColor('b'));
+}
 
 int main() {
 	particle_test();
