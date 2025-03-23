150f5b2a41f8bfa5e45b527406cdb16d33c1c4ac
RedTeamC2 <1005519@my.scccd.edu>
2025-03-22 18:41:21 -0700

    wasserman fixes

--- particle_graphics.cc
+++ particle_graphics.cc
@@ -1,63 +1,64 @@
 #include "particle_graphics.h"
 #include <iostream>
-
+#include <cassert>
+using namespace std;
 
 ParticleGraphics::ParticleGraphics(int red, int green, int blue) : r(red), g(green), b(blue) {}
 
 int ParticleGraphics::getColor(char c) const {
-	if (c == 'r') return r;
-	else if ( c == 'g') return g;
-	else return b;
+    if (c == 'r') return r;
+    else if (c == 'g') return g;
+    else return b;
 }
 
 void ParticleGraphics::setColor(int red, int green, int blue) {
-	r = red;
-	g = green;
-	b = blue;
+    r = red;
+    g = green;
+    b = blue;
 }
 
 void ParticleGraphics::drawCircle(int x, int y, int radius, int red, int green, int blue) {
-	if (radius <= 0) return;
-	setcolor(red, green, blue);
-	for (int row = x - radius; row <= x + radius; row++) {
-		for (int col = y; col <= y + radius; row++) {
-			int dx = row - x;
-			int dy = col - y;
-			if (dx * dx + dy * dy <= radius * radius) {
-				movecursor(row,col);
-				cout << " ";
-			}
-		}
-	}
-
-	cout.flush();
-	resetcolor();
+    if (radius <= 0) return;
+    setbgcolor(red, green, blue);
+    setcolor(0, 0, 0);
+    for (int row = x - radius; row <= x + radius; row++) {
+        for (int col = y - radius; col <= y + radius; col++) {
+            int dx = row - x;
+            int dy = col - y;
+            if (dx * dx + dy * dy <= radius * radius) {
+                movecursor(row, col);
+                cout << " ";
+            }
+        }
+    }
+    cout.flush();
+    resetcolor();
 }
 
 void ParticleGraphics::drawRectangle(int min_x, int min_y, int max_x, int max_y, int red, int green, int blue) {
-	if (min_x >= max_x or min_y >= max_y) return;
-	setcolor(red, green, blue);
-	for (int row = min_x; row <= max_x; row++) {
-		for (int col = min_y; col <= max_y; col++) {
-			movecursor(row,col);
-			cout << " ";
-		}
-	}
-	cout.flush();
-	resetcolor();
+    if (min_x >= max_x || min_y >= max_y) return;
+    setbgcolor(red, green, blue);
+    for (int row = min_x; row <= max_x; row++) {
+        for (int col = min_y; col <= max_y; col++) {
+            movecursor(row, col);
+            cout << " ";
+        }
+    }
+    cout.flush();
+    resetcolor();
 }
 
-void ParticleGraphics::drawLine(int x1, int x2, int y1, int y2, int red, int green, int blue) {
-	assert(x1 >= 0 && x2 >= 0 && y1 >= 0 && y2 >= 0);
-	assert((x1 == x2) || (y1 == y2));
-	setcolor(red, green, blue);
+void ParticleGraphics::drawLine(int x1, int y1, int x2, int y2, int red, int green, int blue) {
+    //assert(x1 >= 0 && x2 >= 0 or y1 >= 0 && y2 >= 0);
+    //assert((x1 == x2) || (y1 == y2)); // Ensure the line is either vertical or horizontal
+    setbgcolor(red, green, blue);
 
     if (x1 == x2) { // Vertical Line
         int y_start = (y1 < y2) ? y1 : y2;
         int y_end = (y1 > y2) ? y1 : y2;
         for (int y = y_start; y <= y_end; y++) {
             movecursor(x1, y);
-            cout << " ";
+            std::cout << " ";
         }
     } else if (y1 == y2) { // Horizontal Line
         int x_start = (x1 < x2) ? x1 : x2;
@@ -72,14 +73,12 @@ void ParticleGraphics::drawLine(int x1, int x2, int y1, int y2, int red, int gre
     resetcolor();
 }
 
-
 void ParticleGraphics::drawPoint(int row, int col, int red, int green, int blue) {
-	setcolor(red, green, blue);
-	movecursor(row,col);
-	cout << " ";
-	cout.flush();
-	resetcolor();
-
+    setbgcolor(red, green, blue);
+    movecursor(row, col);
+    cout << " ";
+    cout.flush();
+    resetcolor();
 }
 
 
--- test_particle_graphics.cc
+++ test_particle_graphics.cc
@@ -1,34 +1,33 @@
 #include "particle_graphics.h"
-#include "colors.h"
-//#include <uistd.h>
+#include <iostream>
+
+using namespace std;
+
+
 
 int main() {
+    clearscreen();
 
-	clearscreen();
+    ParticleGraphics pg(255, 255, 255); // Initial color for particles
 
-	ParticleGraphics pg;
+    // Test draw point
+    pg.drawPoint(5, 5, 255, 0, 0);
 
-	//auto [rows, cols] = get_terminal_size();
-    //cout << "Terminal size: " << rows << " rows x " << cols << " cols\n";
-	//test draw point
-	pg.drawPoint(5, 5, 255, 0, 0);
+    // Test draw rectangle
+    pg.drawRectangle(7, 10, 12, 20, 0, 255, 0);
 
-	//test draw rect
-	pg.drawRectangle(7,10,12,20,0,255,0);
+    // Test draw vertical line
+    pg.drawLine(17, 5, 17, 25, 255, 255, 0);
 
-	//test draw line
-	pg.drawLine(17, 5, 25, 5, 255, 255, 0);
+    // Test draw horizontal line
+    pg.drawLine(25, 15, 30, 15, 255, 255, 0);
 
-	//test draw line horizontal
-	pg.drawLine(25, 15, 25, 20, 255, 255, 0);
+    // Test draw circle
+    pg.drawCircle(10, 50, 5, 255, 0, 255);
 
-	// test circle
-	pg.drawCircle(10,50,5,255,0,255);
+    movecursor(25, 0);
+    resetcolor();
+    cout << endl;
 
-	movecursor(25,0);
-	resetcolor();
-	cout << endl;
-
-	return 0;
-
+    return 0;
 }
